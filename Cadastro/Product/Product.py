3587c86d5fed766971ae70cb8ed7434ddac96bdf
Alessandro Jeferson Carvalho <ale@pop-os.localdomain>
2021-04-08 16:43:43 -0300

    resolução de conflitos

--- Cadastro/Product/Product.py
+++ Cadastro/Product/Product.py
@@ -1,2 +1,23 @@
-class Product():
-    
+
+# prod = open('Produto', 'r', encoding="utf8")
+# produto_nome = input('Por favor digite o seu nome do produto: ')
+# rod_nome = produto_nome.readlines()
+# prod_nome.append(produto_nome)
+# print('O nome do produto é: ', produto_nome.format())
+
+
+
+file = open("Produtos", 'r', encoding="utf8")
+produto_nome = str(input('Por favor digite o seu nome do produto: '))
+
+for line in file:
+    if produto_nome in line:
+        line.append(produto_nome)
+        #print('line: ', produto_nome.format())
+        break
+file.close() 
+
+leitura = open("Produtos", 'a', encoding="utf8")
+leitura.writelines(produto_nome)
+print('line: ', produto_nome.format())
+file.close()  
