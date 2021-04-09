50e89a00dfa94ea6ffe401191a375775bbb2e3ad
Alessandro Jeferson Carvalho <ale@pop-os.localdomain>
2021-04-08 21:21:49 -0300

    criação crud produto

--- Cadastro/Product/Product.py
+++ Cadastro/Product/Product.py
@@ -1,23 +1,109 @@
+def menu_opcoes():
+    print("\n" * 100)
+    while True:
+        print("\n      OPÇÕES DISPONÍVEIS NO BD:\n          1 - INSERIR\n          2 - PESQUISAR\n          3 - ALTERAR\n          4 - EXCLUIR\n          5 - LISTAR\n          6 - SAIR\n")
+        opcao = int(input("DÍGITE SUA OPÇÃO > "))
+        if opcao == 1:
+            inserir()
+        elif opcao == 2:
+            pesquisar()
+        elif opcao == 3:
+            alterar()
+        elif opcao == 4:
+            excluir()
+        elif opcao == 5:
+            listar()
+        elif opcao == 6:
+            print("\n" * 100)
+            print("\n\n>>>>>>> VOCÊ SAIU DO BANCO DE DADOS! <<<<<<<")
+        else:
+            print("\nOpção inválida, escolhas uma das 6 opções disponíveis para continuar!\n")
 
-# prod = open('Produto', 'r', encoding="utf8")
-# produto_nome = input('Por favor digite o seu nome do produto: ')
-# rod_nome = produto_nome.readlines()
-# prod_nome.append(produto_nome)
-# print('O nome do produto é: ', produto_nome.format())
 
+def inserir():
+    #print("\n"*100)
+    #print("\n     --- INSERÇÃO DE NOVO PRODUTO NO BANCO DE DADOS ---\n")
+    arquivo = open('produtos.txt','a+')
+    nome = str(input("Digite o nome do Produto: ")).upper()
 
+    arquivo.writelines("Produto.............: ")
+    arquivo.writelines(str(nome) + "\n")
 
-file = open("Produtos", 'r', encoding="utf8")
-produto_nome = str(input('Por favor digite o seu nome do produto: '))
+    arquivo.write("\n----------------------------------------------------\n")
+    arquivo.close()
+    print("\n----------------------------------------------------\n            PRODUTO CADASTRADO COM SUCESSO!\n----------------------------------------------------\n")
+    continuar_cadastro = str(input("Desenha cadastrar outro produto (S/N)? >>> ")).upper()
+    if continuar_cadastro == "S":
+        inserir()
+    elif continuar_cadastro == "N":
+        continuar_programa()
+    
 
-for line in file:
-    if produto_nome in line:
-        line.append(produto_nome)
-        #print('line: ', produto_nome.format())
-        break
-file.close() 
+def listar():
+    arquivo = open('produtos.txt','r')
+    a = arquivo.readlines()
+    for linha in a:
+        linha = linha.rstrip()
+        print(linha)
+    arquivo.close()
 
-leitura = open("Produtos", 'a', encoding="utf8")
-leitura.writelines(produto_nome)
-print('line: ', produto_nome.format())
-file.close()  
+def pesquisar():
+    arquivo = open('produtos.txt','r')
+    a = arquivo.readlines()
+    prod = input("\nDígite produto: ")
+    for line in a:
+        if prod in line:
+            posicao = (a.index(line))
+            print("\n" + a[prod].rstrip())
+            print("Resultado da pesquisa: "+prod)
+            
+    print("\nDeseja realizar uma nova pesquisa?") 
+    cont = int(input("1 - SIM\n2 - MENU PRINCIPAL\nDigite sua escolha: "))              
+    if cont == 1:
+        pesquisar()
+    elif cont == 2:
+        menu_opcoes()
+    else:
+        print("Valor inválido, escolha uma das duas opções disponíveis")
+
+def excluir():
+    arquivo = open('produtos.txt', 'r+')
+    a = arquivo.readlines()
+    prod = str(input("Dígite o nome do produto que deseja excluir do banco de dados: ")).upper()
+    for line in a:
+        if prod in line:
+            posicao_prod = (a.index(line))
+            a[posicao_prod] = ""
+            arquivo = open('produtos.txt','w')
+            arquivo.writelines(a)
+    arquivo.close()
+
+def continuar_programa():
+    while True:
+        print("\n>>> Dígite 1 para ir para o menu principal.")
+        print(">>> Dígite 2 para sair do Banco de dados.")
+        continuar = int(input("Dígite sua opção: "))
+        if continuar == 1:
+            print("\n"*100)
+            menu_opcoes()
+        elif continuar == 2:
+            print("\n"*100)
+            print("\n\n>>>>>>> VOCÊ SAIU DO BANCO DE DADOS! <<<<<<<")
+            break
+        else:
+             print("Valor inválido, digite 1 = Sim ou 2 = Não.")
+
+def alterar():
+    arquivo = open('produtos.txt','r+')
+    a = arquivo.readlines()
+    prod = str(input("Dígite o nome do produto que deseja fazer alterações: ")).upper()
+    arquivo = open('produtos.txt', 'w')
+    for line in a:
+        if prod in line:
+            posicao_prod = (a.index(line))
+            novo_nome = str(input("Dígite o novo nome do produto que deseja incluir: ")).upper()
+           
+            a[posicao_prod] = (novo_nome)+"\n"
+            arquivo.writelines(a)
+    arquivo.close()
+menu_opcoes()
